444508084a767b4be257381a6f821f3453a86885
Ryan Yep <ryan.yep@gmail.com>
2015-10-05 16:15:16 -0500

    Add missing semicolons in JS code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,11 @@
+// prints Hello World!
 var printer = {
   phrase: 'Hello World!'
 };
 
 // console.log(printer.phrase);
 
+// prints a number, Fizz, Buzz or FizzBuzz
 var fb = function(n) {
   if (n % 15 === 0) {
     console.log('FizzBuzz');
@@ -14,14 +16,15 @@ var fb = function(n) {
   } else {
       console.log(n);
   }
-}
+};
 
+// checks to see if someone is the age of 21
 var age_check = function(age) {
   if (age < 21) {
     console.log('too young');
   } else {
     console.log('proceed');
   }
-}
+};
 age_check(19);
 age_check(22);
