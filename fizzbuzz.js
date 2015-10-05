964c5330b33c398b1cf7def64bf24f90a7a89212
Ryan Yep <ryan.yep@gmail.com>
2015-10-05 16:16:31 -0500

    Re-ordered fizzbuzz objects in JS file

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -9,10 +9,10 @@ var printer = {
 var fb = function(n) {
   if (n % 15 === 0) {
     console.log('FizzBuzz');
-  } else if (n % 3 === 0) {
-      console.log('Fizz');
   } else if (n % 5 === 0) {
       console.log('Buzz');
+  } else if (n % 3 === 0) {
+      console.log('Fizz');
   } else {
       console.log(n);
   }
