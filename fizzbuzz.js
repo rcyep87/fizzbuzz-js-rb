d8f8a6852f42cc80a768be33ca16c23cb28e467b
Ryan Yep <ryan.yep@gmail.com>
2015-10-05 15:32:56 -0500

    FizzBuzz completed in JS

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,12 +5,15 @@ var printer = {
 console.log(printer.phrase);
 
 var fb = function(n) {
-  if (n % 3 === 0) {
-    console.log('Fizz');
+
+  if (n % 15 === 0) {
+    console.log('FizzBuzz');
+  } else if (n % 3 === 0) {
+      console.log('Fizz');
   } else if (n % 5 === 0) {
-    console.log('Buzz');
+      console.log('Buzz');
   } else {
-    console.log(n);
+      console.log(n);
   }
 }
 
@@ -18,3 +21,7 @@ fb(3);
 fb(4);
 fb(5);
 fb(6);
+fb(15);
+fb(16);
+fb(17);
+fb(30);
