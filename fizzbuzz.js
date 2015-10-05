7a69edb74c1c8b3104d50d2db4b36a057eaed4ab
Ryan Yep <ryan.yep@gmail.com>
2015-10-05 15:28:44 -0500

    Fizz & Buzz work in JS

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,10 +7,14 @@ console.log(printer.phrase);
 var fb = function(n) {
   if (n % 3 === 0) {
     console.log('Fizz');
+  } else if (n % 5 === 0) {
+    console.log('Buzz');
   } else {
     console.log(n);
   }
 }
 
 fb(3);
+fb(4);
 fb(5);
+fb(6);
