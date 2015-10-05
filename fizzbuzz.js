661a5a3dc3c7f20379773df887ec6205215553b4
Ryan Yep <ryan.yep@gmail.com>
2015-10-05 15:11:22 -0500

    Working Fizz for JS and Ruby

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,16 @@
 var printer = {
   phrase: 'Hello World!'
 };
+
 console.log(printer.phrase);
+
+var fb = function(n) {
+  if (n % 3 === 0) {
+    console.log('Fizz');
+  } else {
+    console.log(n);
+  }
+}
+
+fb(3);
+fb(5);
--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,21 @@
+# prints "Hello World!"
 puts 'Hello World!'
+
+def run_fb(n)
+  fizz(buzz(fizzbuzz(n)))
+end
+
+def fizz(n)
+  return 'Fizz' if (n % 3).zero?
+  return n
+end
+
+def buzz(n)
+  return 'Buzz' if (n % 5).zero?
+  return n
+end
+
+def fizzbuzz(n)
+  return 'FizzBuzz' if (n % 15).zero?
+  return n
+end
